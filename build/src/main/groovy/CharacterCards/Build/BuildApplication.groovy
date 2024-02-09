370eabafe693853c3c6aac468d886994e28b9af4
Bradley James Barfoot <brbar@netcompany.com>
2024-02-09 14:11:18 +0000

    all

---
+++ build/src/main/groovy/CharacterCards/Build/BuildApplication.groovy
@@ -0,0 +1,13 @@
+package CharacterCards.Build
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BuildApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BuildApplication, args)
+	}
+
+}
---
+++ build/src/test/groovy/CharacterCards/Build/BuildApplicationTests.groovy
@@ -0,0 +1,13 @@
+package CharacterCards.Build
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
